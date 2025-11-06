25e391d844ee3847993e853d44ccd3381cfebdc6
gh-larissaventura <larissa.mirandaventura@gmail.com>
2025-11-06 21:03:25 +1300

    criando pasta app e arquivo main.py

---
+++ app/main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, EmailStr
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import re 
